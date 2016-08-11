9089fba33a643f3fa8cf984c8e6b5d7fc261ca1d
Cepr0 <cepr0@ukr.net>
2016-08-11 23:14:11 +0300

    HW0 - getFilteredWithExceeded() with loops refactoring

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -4,16 +4,14 @@ import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
 import java.time.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
         List<UserMeal> mealList = Arrays.asList(
                 new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 10, 10, 0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 10, 11, 0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 10, 20, 0), "Ужин", 600),
+                new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 10, 13, 0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 10, 20, 0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 11, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 11, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2016, Month.AUGUST, 11, 20, 0), "Ужин", 510)
@@ -23,46 +21,29 @@ public class UserMealsUtil {
         for (UserMealWithExceed meal : filteredMeal) {
             System.out.println(meal);
         }
-//        .toLocalDate();
-//        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed> getFilteredWithExceeded(
-            List<UserMeal> mealList
-            , LocalTime startTime
-            , LocalTime endTime
-            , int caloriesPerDay) {
-        List<UserMealWithExceed> filteredMealList = new ArrayList<>();
+    public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        List<UserMealWithExceed> filteredMeals = new ArrayList<>();
+        Map<LocalDate, Integer> totalCaloriesPerDay = new HashMap<>();
 
-        LocalDate currentDate = LocalDate.now(); // current date
-        LocalDateTime startDateTime = LocalDateTime.of(currentDate, startTime); //start date-time
-        LocalDateTime endDateTime = LocalDateTime.of(currentDate, endTime); //end date-time
-
-        int todayCaloriesCount = 0;
         for (UserMeal curMeal : mealList) {
-            LocalDateTime userMealDateTime = curMeal.getDateTime();
-            Period period = Period.between(currentDate, userMealDateTime.toLocalDate());
+            LocalDate localDate = curMeal.getDateTime().toLocalDate();
+            int totalCalories = totalCaloriesPerDay.containsKey(localDate)
+                    ? totalCaloriesPerDay.get(localDate) + curMeal.getCalories()
+                    : curMeal.getCalories();
+            totalCaloriesPerDay.put(localDate, totalCalories);
+        }
 
-            // if period between today and current meal day is 0 then count calories total of today
-            if (Period.between(currentDate, userMealDateTime.toLocalDate()).getDays() == 0) {
-                todayCaloriesCount = todayCaloriesCount + curMeal.getCalories();
+        for (UserMeal curMeal : mealList) {
+            if (TimeUtil.isBetween(curMeal.getDateTime().toLocalTime(), startTime, endTime)) {
+                filteredMeals.add(new UserMealWithExceed(
+                        curMeal
+                        , (totalCaloriesPerDay.get(curMeal.getDateTime().toLocalDate()) > caloriesPerDay))
+                );
             }
         }
 
-        if (todayCaloriesCount > caloriesPerDay) {
-            for (UserMeal curMeal : mealList) {
-                LocalDateTime userMealDateTime = curMeal.getDateTime();
-
-                // if current meal date-time is after or equal of filter start time
-                // and if it date-time is before or equal of filter end time
-                if ((userMealDateTime.isAfter(startDateTime) || userMealDateTime.isEqual(startDateTime))
-                        && (userMealDateTime.isBefore(endDateTime) || userMealDateTime.isEqual(endDateTime))) {
-                    // then add it to result list
-                    filteredMealList.add(new UserMealWithExceed(curMeal, true));
-                }
-            }
-        }
-
-        return filteredMealList;
+        return filteredMeals;
     }
 }
