e27212f95fdc25ee9bd94e19c47510812c1967bf
Cepr0 <cepr0@ukr.net>
2016-08-27 21:00:35 +0300

    finished tests

--- src/test/ru/javawebinar/topjava/util/UserMealsUtilTest.java
+++ src/test/ru/javawebinar/topjava/util/UserMealsUtilTest.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.util;
 
-import org.junit.*;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
@@ -15,161 +17,165 @@ public class UserMealsUtilTest {
     // База с тестовыми данными: [кол-во данных] - [список с даными]
     private static Map<Integer, List<UserMeal>> testData = new TreeMap<>();
 
-    // База с результатами: [номер теста] - [данные с результатами]
-    private static Map<Integer, List<UserMealWithExceed>> resultData = new TreeMap<>();
+    // База результатов тестов (см. вл. класс ResultData ниже)
+    private static List<ResultData> resultData = new ArrayList<>();
 
-    // Базы измерений времени выполнения тестов (см. вн. класс MeasurementData ниже)
-    private static List<MeasurementData> measurements = new ArrayList<>();
+    // Мапа обрабатываемых тестов: [номер теста] - [название]
+    private static Map<Integer, String> variants = new TreeMap<>();
 
-    private static List<Instant> beforeList = new ArrayList<>();
-    private static List<Instant> afterList = new ArrayList<>();
+    // Массив, в котором указываем кол-во данных для тестов (они же - индексы тестовых наборов данных)
+    private static int[] counts = {1_000, 5_000, 10_000, 50_000, 100_000, 500_000};
 
-    // Мапа с названиями тестов: [номер теста] - [название]
-    private static Map<Integer, String> variantNames = new TreeMap<>();
+    private static final int TIMEOUT = 300_000; // Значение timeout для аннотации @Test
 
+    // Заполнем базу тестовыми данными
     @BeforeClass
     public static void setUp() throws Exception {
         LocalDate date = LocalDate.of(2012, Month.JANUARY, 1);
         Random r = new Random();
-        List<UserMeal> mealList = new ArrayList<>();
-
-        // Заполнем тестовыми данными
-        // Массив, в котором указываем кол-во данных для тестов (они же - номер тестового набора данных)
-        int[] counts = {1_000, 5_000, 10_000, 50_000, 100_000, 500_000};
+        List<UserMeal> mealList;
 
         for (int count : counts) {
             // Генерируем данные
+            mealList = new ArrayList<>();
             for (int i = 0; i < count; i++) {
                 date = date.plus(1, ChronoUnit.DAYS);
                 mealList.add(new UserMeal(LocalDateTime.of(date, LocalTime.of(10, 0)), "Завтрак", 300 + r.nextInt(400)));
                 mealList.add(new UserMeal(LocalDateTime.of(date, LocalTime.of(13, 0)), "Обед", 800 + r.nextInt(400)));
                 mealList.add(new UserMeal(LocalDateTime.of(date, LocalTime.of(20, 0)), "Ужин", 300 + r.nextInt(400)));
             }
-            // И сохраняем в базу
+            // Сохраняем в базу
             testData.put(count, mealList);
         }
 
-        // греем Джаву на тетьем наборе данных и на 4-х тестах (3 тест заранее долгий - пропускаем)
-        UserMealsUtil.getFilteredWithExceeded0(testData.get(2), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
-        UserMealsUtil.getFilteredWithExceeded1(testData.get(2), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
-        UserMealsUtil.getFilteredWithExceeded2(testData.get(2), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
-        UserMealsUtil.getFilteredWithExceeded4(testData.get(2), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
+        // греем Джаву на третьем наборе данных и на 4-х тестах (3-й тест заранее долгий - пропускаем)
+        UserMealsUtil.getFilteredWithExceeded0(testData.get(counts[2]), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
+        UserMealsUtil.getFilteredWithExceeded1(testData.get(counts[2]), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
+        UserMealsUtil.getFilteredWithExceeded2(testData.get(counts[2]), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
+        UserMealsUtil.getFilteredWithExceeded4(testData.get(counts[2]), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
 
-        // Выводим хедер таблицы результатов
-        System.out.println("Variant        |   ML size | MLWE size | exc. count | Duration, ms");
+        // Выводим заголовок таблицы результатов
+        System.out.println("Variant        |   UM size | UMWE size | exc. count | Duration, ms");
     }
 
     @AfterClass
-    public static void tearDown() throws Exception {
-        // Выводим результаты
-        List<Long> exceedCounts = new ArrayList<>();
-        List<Long> durations = new ArrayList<>();
+    public static void printResults() throws Exception {
 
-        // Цикл по кол-ву вариантов
-        for (int i = 0; i < variantNames.size(); i++) {
+        // Цикл по протестированным вариантам
+        for (Map.Entry<Integer, String> varEntry : variants.entrySet()) {
+            final int curVariant = varEntry.getKey();
 
+            // Цикл по наборам данных
             for (int j = 0; j < testData.size(); j++) {
-                exceedCounts.add(resultData.get(j).stream().filter(UserMealWithExceed::isExceed).count());
-                durations.add(Duration.between(beforeList.get(j), afterList.get(j)).toMillis());
-            }
+                final int curDataSetNum = counts[j];
 
-            for (int j = 0; j < testData.size(); j++) {
+                // Получаем текущий набор данных
+                ResultData curResult = resultData.stream()
+                        .filter(result -> result.testNum == curVariant && result.dataSetNum == curDataSetNum)
+                        .findFirst()
+                        .orElse(null);
+
+                // Считаем кол-во объектов UserMealWithExceed у которых exceed == true
+                long exceedCount = (curResult != null && curResult.data != null)
+                        ? curResult.data.stream().filter(UserMealWithExceed::isExceed).count()
+                        : -1L;
+
+                // Получаем длительность теста
+                long duration = (curResult != null) ? curResult.getDuration() : -1L;
+
+                // Определяем кол-во всего объектов UserMealWithExceed
+                int exceedListSize = (curResult != null && curResult.data != null) ? curResult.data.size() : -1;
+
+                // Выводим результат
                 System.out.printf("%-14s | %9d | %9d | %10d | %12d\n"
-                        , variantNames, testData.get(j).size(), resultData.get(j).size(), exceedCounts.get(j), durations.get(j));
+                        , varEntry.getValue(), testData.get(counts[j]).size(), exceedListSize, exceedCount, duration);
             }
+            System.out.println("------------------------------------------------------------------");
         }
-        // "Освождаем" память
+
+        // "Освобождаем" память
         testData.clear();
         resultData.clear();
     }
 
-    @Before
-    public void before() {
-        // Разделитель опытов
-        System.out.println("------------------------------------------------------------------");
-    }
+//    @Before
+//    public void before() {
+//    }
 
-    @After
-    public void after() {
-    }
+//    @After
+//    public void after() {
+//    }
 
-    private class MeasurementData {
-        int testNum;
-        int dataSetNum;
+    private class ResultData {
+        int testNum; // Номер теста
+        int dataSetNum; // Номер набора тестовых данных
         Instant start;
         Instant end = null;
+        List<UserMealWithExceed> data = null; // результирующий список объектов UserMealWithExceed
 
-        public MeasurementData(int testNum, int dataSetNum, Instant start) {
+        ResultData(int testNum, int dataSetNum, Instant start) {
             this.testNum = testNum;
             this.dataSetNum = dataSetNum;
             this.start = start;
         }
 
-        public void setEnd(Instant end) {
-            this.end = end;
-        }
-
-        public int getTestNum() {
-            return testNum;
-        }
-
-        public int getDataSetNum() {
-            return dataSetNum;
-        }
-
-        public long getDuration() {
-            return (end != null) ? Duration.between(start, end).toMillis() : -1;
+        long getDuration() {
+            return (start != null && end != null) ? Duration.between(start, end).toMillis() : -1;
         }
     }
 
+
+    // Основной метод для проведения теста на нескольких наборах данных
     private void runTest(int testNum, String testName) {
-        variantNames.put(testNum, testName);
+        variants.put(testNum, testName);
 
         for (Map.Entry<Integer, List<UserMeal>> entry : testData.entrySet()) {
-            MeasurementData measurementData = new MeasurementData(testNum, entry.getKey(), Instant.now());
-            measurements.add(measurementData);
+            int dataSetNum = entry.getKey();
+            ResultData result = new ResultData(testNum, dataSetNum, Instant.now());
+            resultData.add(result);
+            List<UserMeal> testDataSet = entry.getValue();
             switch (testNum) {
                 case 0:
-                    resultData.put(testNum, UserMealsUtil.getFilteredWithExceeded0(entry.getValue(), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000));
+                    result.data = UserMealsUtil.getFilteredWithExceeded0(testDataSet, LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
                     break;
                 case 1:
-                    resultData.put(testNum, UserMealsUtil.getFilteredWithExceeded1(entry.getValue(), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000));
+                    result.data = UserMealsUtil.getFilteredWithExceeded1(testDataSet, LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
                     break;
                 case 2:
-                    resultData.put(testNum, UserMealsUtil.getFilteredWithExceeded2(entry.getValue(), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000));
+                    result.data = UserMealsUtil.getFilteredWithExceeded2(testDataSet, LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
                     break;
                 case 3:
-                    resultData.put(testNum, UserMealsUtil.getFilteredWithExceeded3(entry.getValue(), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000));
+                    result.data = UserMealsUtil.getFilteredWithExceeded3(testDataSet, LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
                     break;
                 case 4:
-                    resultData.put(testNum, UserMealsUtil.getFilteredWithExceeded4(entry.getValue(), LocalTime.of(7, 0), LocalTime.of(13, 0), 2000));
+                    result.data = UserMealsUtil.getFilteredWithExceeded4(testDataSet, LocalTime.of(7, 0), LocalTime.of(13, 0), 2000);
                     break;
             }
-            measurementData.setEnd(Instant.now());
+            result.end = Instant.now();
         }
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void getFilteredWithExceeded0() throws Exception {
         runTest(0, "O(2n) cycle");
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void getFilteredWithExceeded1() throws Exception {
         runTest(1, "O(1.5n) cycle");
     }
 
-    @Test
+    @Test (timeout = TIMEOUT)
     public void getFilteredWithExceeded2() throws Exception {
         runTest(2, "O(2n) stream");
     }
 
-    @Test(timeout = 60000)
+    @Test(timeout = TIMEOUT)
     public void getFilteredWithExceeded3() throws Exception {
         runTest(3, "O(n^2?) stream");
     }
 
-    @Test
+    @Test(timeout = TIMEOUT)
     public void getFilteredWithExceeded4() throws Exception {
         runTest(4, "O(1.5n) stream");
     }
