3f4aec3a067b28a0231a29613af1cdfd51367621
Cepr0 <cepr0@ukr.net>
2016-09-24 00:08:28 +0300

    lesson03

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -6,15 +6,13 @@ import ru.javawebinar.topjava.model.User;
 
 import java.util.Objects;
 
-import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
-
 /**
  * GKislin
  * 24.09.2015.
  */
 public class UserTestData {
-    public static final int USER_ID = START_SEQ;
-    public static final int ADMIN_ID = START_SEQ + 1;
+    public static final int USER_ID = 100;//START_SEQ;
+    public static final int ADMIN_ID = USER_ID + 1;
 
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN);
