3b6e736955de610e35ea6faf1a0aee7977b7f48c
Cepr0 <cepr0@ukr.net>
2016-10-09 02:03:33 +0300

    hw05 basic part completed

---
+++ src/test/java/ru/javawebinar/topjava/service/JpaMealServiceTest.java
@@ -0,0 +1,62 @@
+package ru.javawebinar.topjava.service;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.Profiles;
+
+/**
+ * @author Cepro
+ *         09.10.2016
+ */
+
+@ActiveProfiles(Profiles.JPA)
+public class JpaMealServiceTest extends MealServiceTest {
+  @Override
+  public void setUp() throws Exception {
+    super.setUp();
+  }
+  
+  @Override
+  public void testDelete() throws Exception {
+    super.testDelete();
+  }
+  
+  @Override
+  public void testDeleteNotFound() throws Exception {
+    super.testDeleteNotFound();
+  }
+  
+  @Override
+  public void testSave() throws Exception {
+    super.testSave();
+  }
+  
+  @Override
+  public void testGet() throws Exception {
+    super.testGet();
+  }
+  
+  @Override
+  public void testGetNotFound() throws Exception {
+    super.testGetNotFound();
+  }
+  
+  @Override
+  public void testUpdate() throws Exception {
+    super.testUpdate();
+  }
+  
+  @Override
+  public void testNotFoundUpdate() throws Exception {
+    super.testNotFoundUpdate();
+  }
+  
+  @Override
+  public void testGetAll() throws Exception {
+    super.testGetAll();
+  }
+  
+  @Override
+  public void testGetBetween() throws Exception {
+    super.testGetBetween();
+  }
+}
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -23,7 +23,10 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
@@ -35,21 +38,27 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringJUnit4ClassRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles({Profiles.ACTIVE_DB, Profiles.ACTIVE_REPO})
-public class MealServiceTest {
+@ActiveProfiles(Profiles.ACTIVE_DB)
+public abstract class MealServiceTest {
   private static final Logger LOG = LoggerFactory.getLogger(MealServiceTest.class);
   
   @Rule
   public ExpectedException thrown = ExpectedException.none();
   
-  private static StringBuffer results = new StringBuffer();
+  private static final Map<String, Long> testDurations = new HashMap<>();
   
   @AfterClass
   public static void printResult() {
-    System.out.printf("%nTest             Duration, ms%n");
-    System.out.println("-----------------------------");
-    System.out.println(results);
-    System.out.printf("-----------------------------%n%n");
+    if (!testDurations.isEmpty()) {
+      Stream<String> stream = testDurations.entrySet().stream()
+          .sorted((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
+          .map(e -> String.format("%-20s %8d", e.getKey(), e.getValue()));
+  
+      System.out.printf("%nTest             Duration, ms%n");
+      System.out.println("-----------------------------");
+      stream.forEach(System.out::println);
+      System.out.printf("-----------------------------%n%n");
+    }
   }
   
   @Rule
@@ -57,9 +66,11 @@ public class MealServiceTest {
   public Stopwatch stopwatch = new Stopwatch() {
     @Override
     protected void finished(long nanos, Description description) {
-      String result = String.format("%-20s %8d", description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-      results.append(result).append('\n');
-      LOG.info(result + " ms\n");
+      String testName = description.getMethodName();
+      long duration = TimeUnit.NANOSECONDS.toMillis(nanos);
+      String result = String.format("*** %s duration is %d ms", testName, duration);
+      LOG.info(result);
+      testDurations.put(testName, duration);
     }
   };
   
