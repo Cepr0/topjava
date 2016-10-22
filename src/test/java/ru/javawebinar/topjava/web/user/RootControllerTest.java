08f593391a913e925f9a018a074f55d61237b680
Cepr0 <cepr0@ukr.net>
2016-10-22 22:20:54 +0300

    hw07 1.1

--- src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/RootControllerTest.java
@@ -1,12 +1,14 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.junit.Test;
+import org.springframework.http.MediaType;
 import ru.javawebinar.topjava.web.AbstractControllerTest;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
@@ -15,7 +17,32 @@ import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
  * 10.04.2015.
  */
 public class RootControllerTest extends AbstractControllerTest {
+    @Test
+    public void testResources() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+               .andDo(print())
+               .andExpect(content().contentType(MediaType.valueOf("text/css")))
+               .andExpect(status().isOk());
+    }
 
+    @Test
+    public void testsMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+               .andDo(print())
+               .andExpect(status().isOk())
+               .andExpect(view().name("meals"))
+               .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+               .andExpect(model().attribute("meals", hasSize(6)))
+               .andExpect(model().attribute("meals", hasItem(
+                       allOf(
+                               hasProperty("id", is(MEAL1_ID)),
+                               hasProperty("description", is(MEAL1.getDescription()))))))
+               .andExpect(model().attribute("meals", hasItem(
+                       allOf(
+                               hasProperty("id", is(MEAL1_ID + 1)),
+                               hasProperty("description", is(MEAL2.getDescription()))))));
+    }
+    
     @Test
     public void testUsers() throws Exception {
         mockMvc.perform(get("/users"))
