bfb4f8843c2c78d909671268a505dee273886611
Cepr0 <cepr0@ukr.net>
2016-10-26 23:53:05 +0300

    playing with SpringActiveProfileResolver

--- src/main/java/ru/javawebinar/topjava/SpringActiveProfileResolver.java
+++ src/main/java/ru/javawebinar/topjava/SpringActiveProfileResolver.java
@@ -1,16 +1,29 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ActiveProfilesResolver;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 /**
  * @author Cepro
  *         19.10.16
  */
 public class SpringActiveProfileResolver implements ActiveProfilesResolver {
+    private static final Logger LOG = LoggerFactory.getLogger(SpringActiveProfileResolver.class);
     
     @Override
     public String[] resolve(final Class<?> aClass) {
-        final String activeDBProfile = System.getProperty("spring.profiles.active");
-        return new String[]{activeDBProfile == null ? Profiles.HSQLDB : activeDBProfile};
+        final String property = System.getProperty("spring.profiles.active");
+        String[] profiles;
+        if (property != null) {
+            profiles = property.split(",");
+        } else {
+            profiles = new String[]{Profiles.HSQLDB, Profiles.DATAJPA};
+        }
+        LOG.debug("Active profiles: {}", Arrays.stream(profiles).collect(Collectors.joining(", ")));
+        return profiles;
     }
 }
